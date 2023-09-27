6b4b7bd10b33897b028cf1e0d93f2003855b80a1
alexedozi <chukwujioke60@gmail.com>
2023-09-27 08:14:03 +0200

    initial commit

--- chamber/scripts/chamberDates.js
+++ chamber/scripts/chamberDates.js
@@ -30,3 +30,30 @@ function scrollDown(elem) {
         document.querySelector('#scroll-down').className = ""
     }
 }
+
+
+
+    // Function to calculate the number of days between two dates
+    function daysBetweenDates(date1, date2) {
+        const oneDay = 24 * 60 * 60 * 1000; // One day in milliseconds
+        const diffDays = Math.round(Math.abs((date1 - date2) / oneDay));
+        return diffDays;
+    }
+
+    // Check if this is the user's first visit
+    if (!localStorage.getItem("lastVisit")) {
+        document.querySelector(".sidebar").textContent = "Welcome! Let us know if you have any questions.";
+    } else {
+        const lastVisitDate = new Date(localStorage.getItem("lastVisit"));
+        const currentDate = new Date();
+        const daysSinceLastVisit = daysBetweenDates(currentDate, lastVisitDate);
+
+        if (daysSinceLastVisit === 1) {
+            document.querySelector(".sidebar").textContent = "Back so soon! Awesome!";
+        } else {
+            document.querySelector(".sidebar").textContent = `You last visited ${daysSinceLastVisit} days ago.`;
+        }
+    }
+
+    // Store the current visit date in localStorage
+    localStorage.setItem("lastVisit", new Date().toString());
