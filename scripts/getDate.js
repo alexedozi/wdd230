25fd95880a263aaae82b9d24183cce899092570f
alexedozi <chukwujioke60@gmail.com>
2023-09-27 19:05:41 +0200

    initial commit

--- scripts/getDate.js
+++
@@ -1,6 +0,0 @@
-const currentYear = 2023;
-document.querySelector('#year').textContent = currentYear;
-
-let text = document.lastModified;
-const date = new Date(document.lastModified);
-document.getElementById('currentDate').innerHTML = text;
