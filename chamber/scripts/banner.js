d1a3bc34fb3bb6c4219345decc4309f99791aff4
alexedozi <chukwujioke60@gmail.com>
2023-10-20 11:02:25 +0200

    initial commit

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,15 +1,7 @@
-const today = new Date();
-const dayOfWeek = today.getDay(); // 0 (Sunday) to 6 (Saturday)
+const currDate = Date.now();
+const dt = new Date(currDate);
+const dayValue = dt.getDay();
 
-if (dayOfWeek >= 1 && dayOfWeek <= 3) { // Monday (1) to Wednesday (3)
-  const banner = document.getElementById('meet-greet-banner');
-  banner.style.display = 'block';
-
-  const closeButton = document.getElementById('close-banner-button');
-  closeButton.addEventListener('click', () => {
-    banner.style.display = 'none';
-  });
-} else {
-  const banner = document.getElementById('meet-greet-banner');
-  banner.style.display = 'none';
+if (dayValue <3 && dayValue >0){
+    document.getElementById("banner").className = "visible";
 }
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,24 +1,3 @@
-// Function to display the three-day forecast
-function displayForecast(forecastData) {
-    const forecastList = forecastData.list;
-    const forecastContainer = document.getElementById('forecast-container'); // Get the forecast container by ID
-
-    // Assuming the forecast provides data for the next 3 days in 3-hour increments
-    // You can adjust this logic based on your specific needs
-    const threeDayForecast = forecastList.slice(0, 8); // 8 entries for the next 24 hours
-
-    for (const forecast of threeDayForecast) {
-        const forecastTime = new Date(forecast.dt * 1000); // Convert timestamp to Date object
-        const forecastTemp = forecast.main.temp;
-        const forecastDescription = forecast.weather[0].description;
-
-        const forecastItem = document.createElement('div');
-        forecastItem.classList.add('forecast-item');
-        forecastItem.innerHTML = `<strong>${forecastTime.toLocaleTimeString()}</strong>: ${forecastTemp}&deg;F, ${forecastDescription}`;
-
-        forecastContainer.appendChild(forecastItem);
-    }
-}
 // Select HTML elements in the document
 const currentTemp = document.getElementById('current-temp');
 const weatherCondition = document.getElementById('weather-condition');
